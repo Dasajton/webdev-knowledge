79bdc51950df1859d75acd125695edcde3686513
Dasajton <david.sajitz.94@gmail.com>
2023-12-14 14:09:20 +0100

    import Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-export default function App() {
+import Header from './components/Header'
+
+const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
     </>
   )
 }
+
+export default App
